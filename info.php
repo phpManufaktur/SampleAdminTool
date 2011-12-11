9057647915f27eff9d3c96c4a54339264c6329af
hertsch <ralf.hertsch@phpmanufaktur.de>
2011-12-11 16:49:06 +0000

    git-svn-id: https://server3.subversion-server.com/phpmanufaktur/svn/SampleAdminTool/trunk@2 a95a697e-86d4-604a-9c56-a2f335168923

---
+++ info.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * SampleAdminTool
+ *
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ *
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {
+	if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php');
+} else {
+	$oneback = "../";
+	$root = $oneback;
+	$level = 1;
+	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+		$root .= $oneback;
+		$level += 1;
+	}
+	if (file_exists($root.'/framework/class.secure.php')) {
+		include($root.'/framework/class.secure.php');
+	} else {
+		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+	}
+}
+// end include class.secure.php
+
+$module_directory     = 'sample_admintool';
+$module_name          = 'SampleAdminTool';
+$module_function      = 'tool';
+$module_version       = '0.10';
+$module_status        = 'Beta';
+$module_platform      = '2.8';
+$module_author        = 'Ralf Hertsch, Berlin (Germany)';
+$module_license       = 'GNU General Public License';
+$module_description   = 'Sample for creating a LEPTON Admin-Tool';
+$module_home          = 'http://phpmanufaktur.de';
+$module_guid          = '279BA359-0C85-4950-B631-62C93FADDC23';
+
+?>
---
+++ install.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * SampleAdminTool
+ * 
+ * @author Ralf Hertsch (ralf.hertsch@phpmanufaktur.de)
+ * @link http://phpmanufaktur.de
+ * @copyright 2011
+ * @license GNU GPL (http://www.gnu.org/licenses/gpl.html)
+ * @version $Id$
+ * 
+ * FOR VERSION- AND RELEASE NOTES PLEASE LOOK AT INFO.TXT!
+ */
+
+// include class.secure.php to protect this file and the whole CMS!
+if (defined('WB_PATH')) {    
+    if (defined('LEPTON_VERSION')) include(WB_PATH.'/framework/class.secure.php'); 
+} else {
+    $oneback = "../";
+    $root = $oneback;
+    $level = 1;
+    while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+        $root .= $oneback;
+        $level += 1;
+    }
+    if (file_exists($root.'/framework/class.secure.php')) { 
+        include($root.'/framework/class.secure.php'); 
+    } else {
+        trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+    }
+}
+// end include class.secure.php
+
+global $database;
+global $admin;
+
+$SQL =  "CREATE TABLE IF NOT EXISTS ".TABLE_PREFIX."mod_sample_admintool (".
+        "sample_id INT(11) NOT NULL AUTO_INCREMENT,".
+        "sample_title VARCHAR(255) NOT NULL DEFAULT '',".
+        "sample_text TEXT NOT NULL DEFAULT '',".
+        "PRIMARY KEY (sample_id))";
+
+$database->query($SQL);
+
+if ($database->is_error()) {
+    $admin->print_error('[CREATE TABLE] '.$database->get_error());
+}
